9809a3b3e10c5706a6faf26e862a9f285ab5e360
Cerenda5 <linda.dogan@outlook.fr>
2020-08-19 02:37:14 +0200

    test new structure for authentification with jwt token

--- api/app.js
+++ api/app.js
@@ -1,43 +1,27 @@
-require('dotenv').config(); //Variable environnement
-const express = require ('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors'); //cors fournit un middleware Express pour activer CORS avec diverses options.
-const dbconfig = require('./config/db.config')
-const userRoute = require('./src/routes/user.routes');
-const projectRoutes = require('./src/routes/project.routes');
-
+const express = require('express');
 const app = express();
-let corsOptions = {
-  origin: "http://localhost:8080"
-};
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-//  CORS
-app.use(cors(corsOptions)); // parse requests of content-type - application/json
-app.use(bodyParser.json()); // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+// Import Routes
+const userRoute = require('./routes/user');
 
-console.log(dbconfig.db());
-mongoose.Promise = global.Promise;
-mongoose
-  .connect(dbconfig.db(), {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
+dotenv.config();
 
-userRoute(app);
-projectRoutes(app);
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECT, 
+{ useNewUrlParser: true,
+  useUnifiedTopology: true, 
+  useFindAndModify: true,}, 
+(err, res) => { 
+    if (err) throw err;  
+    console.log('Connected to MongoDb database');
+});
 
-app.listen(process.env.NODE_ENV === "development" ? process.env.DEV_SRV_PORT : process.env.PORT, () =>{
-  if(process.env.NODE_ENV === "development"){
-    console.log('App Listening on Host: ' + process.env.SRV_HOST + ' / Port server: ' + process.env.DEV_SRV_PORT);
-  }
-})
+// Middleware
+app.use(express.json());
+
+//Routes Middlewares
+app.use('/api/user', userRoute);
+
+app.listen(3000, () => console.log('Server up and running'));
