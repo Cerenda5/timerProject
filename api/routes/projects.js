def8551e59e7fd21b9ec8ec4e9774b844fb1b680
Valentin Le Bon <lebonvalentin@hotmail.fr>
2020-08-24 16:22:02 +0200

    Editing Group and Project Model to get Project depends on group

--- api/routes/projects.js
+++ api/routes/projects.js
@@ -4,87 +4,99 @@ const mongoose = require('mongoose');
 
 const checkAuth = require('../middleware/check-auth');
 const Project = require('../models/project');
+const Group = require ('../models/group')
 
 router.get('/', (req, res, next) => {
-   Project.find()
-   .select('name')
-   .exec()
-   .then(docs => {
-       const response = {
-           count: docs.length,
-           projects: docs.map(doc => {
-               return {
-                   _id: doc._id,
-                   name: doc.name,
-                   url: {
+    Project.find()
+    .select('name groupId')
+    .exec()
+    .then(docs => {
+        res.status(200).json({
+            count: docs.length,
+            projects: docs.map(doc => {
+                return {
+                    _id: doc._id,
+                    group: doc.group,
+                    name: doc.name,
+                    request: {
                         type: 'GET',
-                        url: 'http://localhost:3000/projects/' + doc._id //nom du domaine
-                   }
-               }
-           })
-       }
-       res.status(200).json(response)
-   })
-   .catch(err => {
-       console.log(err);
-       res.status(500).json({
-           error: err
-       })
-   });
+                        url: 'http://localhost:3000/projects/' + doc._id
+                    }
+                }
+            })
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
 });
 
-router.post('/' ,(req, res, next) => {
-    const project = new Project({
-        _id: new mongoose.Types.ObjectId(),
-        name: req.body.name
-    });
-    project 
-    .save()
-    .then(result => {
-        console.log(result);
-        res.status(201).json({
-            message:'Project CREATED successfully !',
-            createdProject: {
-                _id: result._id,
-                name: result.name,
+router.post('/', (req, res, next) => {
+    Group.findById(req.body.groupId)
+        //Check we do have a group
+        .then(group => {
+            const project = new Project({
+                _id: mongoose.Types.ObjectId(),
+                group: req.body.groupId,
+                name: req.body.name,
+            });
+            if (!group) {
+                return res.status(404).json({
+                    message: 'Group not found'
+                });
+            }
+            return project.save();
+        })
+        // Execute project creation
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: 'Project created successfully',
+                createdProject: {
+                    _id: result._id,
+                    group: result.group,
+                    name: result.name,
+                },
                 request: {
                     type: 'GET',
-                    description: 'CREATE_PROJECT',
-                    url: 'http://localhost:3000/projects/' + result._id
+                    url: 'http://localhost:3000/groups/' + result._id
                 }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+router.get('/:projectsId', (req, res, next) => {
+    Project.findById(req.params.projectId)
+    .select('name groupId')
+    .exec()
+    .then(project => {
+        if (!project) {
+            return res.status(404).json({
+                message: 'Project not found'
+            })
+        }
+        res.status(200).json({
+            project: project,
+            request: {
+                type: 'GET',
+                url: 'http://localhost:3000/projects/' + id
             }
         });
     })
     .catch(err => {
         console.log(err);
-        res.status(500).json({error: err});
-    });
-});
-
-router.get('/:projectId', (req, res, next) => {
-    const id = req.params.projectId;
-    Project.findById(id)
-    .select('name')
-    .exec()
-    .then(doc => {
-        console.log('From database', doc);
-        if (doc) {
-            res.status(200).json({
-                project: doc,
-                request: {
-                    type: 'GET',
-                    description: 'GET_PROJECT_BY_ID',
-                    url: 'http://localhost:3000/projects/' + doc._id 
-
-                }
-            });
-        } else {
-            res.status(404).json({message: 'No valid entry found for provided ID'});
-        }
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({error: err});
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
@@ -92,9 +104,9 @@ router.put('/:projectId', (req, res, next) => {
     const id = req.params.projectId;
     const updateOps = {};
     for (const ops of req.body) {
-        updateOps[ops.propName] = ops.value;
+        updateOps[ops.propUser] = ops.value;
     }
-    Project.update({ _id: id }, {$set: updateOps})
+    Group.update({ _id: id }, {$set: updateOps})
     .exec()
     .then(result => {
         console.log(result);
@@ -102,7 +114,7 @@ router.put('/:projectId', (req, res, next) => {
             message:'Project UPDATED successfully !',
             request: {
                 type: 'GET',
-                description: 'GET_PROJECT_BY_ID',
+                description: 'GET_Project_BY_ID',
                 url: 'http://localhost:3000/projects/' + id 
             },
         });
@@ -115,8 +127,7 @@ router.put('/:projectId', (req, res, next) => {
 
 
 router.delete('/:projectId', (req, res, next) => {
-    const id = req.params.projectId;
-    Project.remove({ _id: id })
+    Project.remove({ _id: req.params.projectId })
     .exec()
     .then(result => {
         console.log(result);
@@ -126,14 +137,14 @@ router.delete('/:projectId', (req, res, next) => {
                 type: 'POST',
                 url: 'http://localhost:3000/projects/',
                 description: 'You can create a new project with this body :',
-                body: {name: 'String'}
+                body: { groupId: 'ID', name: 'String'}
             },
         });
     })
     .catch(err => {
         console.log(err);
         res.status(500).json({error: err});
-    });
+    })
 });
 
 module.exports = router;
